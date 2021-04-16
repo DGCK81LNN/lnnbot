e592205b84487033993e5b312e60693eabd5011c
DGCK81LNN <triluolnn@163.com>
2021-04-16 23:13:45 +0800

    feat: 配置plugin-common

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,34 @@ const app = new App({
 });
 
 app.plugin(PluginCommon, {
+    respondent: [
+        { match: /\S(\s+\S)+/, reply: "foo" },
+    ],
+    onRepeat: { minTimes: 3 },
+    /*onInterrupt: (state) => (
+        state.repeated &&
+        state.times >= 2 &&
+        Math.random() > 0.5 &&
+        "N" + "o".repeat(state.times) + "!"
+    ),*/
+    onFriendRequest: true,
+    onGroupMemberRequest: async (session) => {
+        var returnValue = undefined;
+        if (
+            session.channelId === "773864545" &&
+            session.content
+                .match(/答案：\s*(.+)\s*$/)[1]
+                .match(/^(?:(?:小马|音游)向?|双厨)$/)
+        )
+            returnValue = true;
+
+        session.send(
+            `收到加群申请，验证消息：\n` +
+            session.content.replace(/^/mg, "> ") + "\n" +
+            `已${returnValue ? "同意" : "忽略"}。`
+        );
+        return returnValue;
+    }
 });
 
 app.start();
