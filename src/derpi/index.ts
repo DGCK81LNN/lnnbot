58a1fb2ec47661fbb0ceb9e106be3848982896b2
DGCK81LNN <triluolnn@163.com>
2022-02-19 20:12:13 +0800

    lnnbot-derpi test

--- src/derpi/index.ts
+++ src/derpi/index.ts
@@ -1,5 +1,8 @@
 import { Context, segment } from "koishi"
-import fetch from "node-fetch"
+import fs from "fs"
+import Stream from "stream"
+import path from "path"
+import axios from "axios"
 
 export const name = "lnnbot-derpi"
 export interface Config {
@@ -7,8 +10,24 @@ export interface Config {
 }
 export function apply(cxt: Context, config: Config = { }) {
   cxt
-    .command("derpi", ">>0")
-    .action(() => {
-      return ">>0\n" + segment("image", { url: "https://derpicdn.net/img/2012/1/2/0/thumb.jpg" })
+    .command("derpi <id:natural>", "获取呆站图片")
+    .action(async (_argv, id) => {
+      var outPath = path.resolve(`./.lnnbot_cache/${id}.image`)
+
+      try {
+        await fs.promises.lstat(outPath)
+      } catch { // not cached
+        await fs.promises.mkdir(path.dirname(outPath), { recursive: true })
+
+        var metadata = (await axios.get(`https://derpibooru.org/api/v1/json/images/${id}`)).data.image
+
+        var stream = (await axios.get(metadata.representations["small"], {
+          responseType: "stream",
+        })).data
+
+        await Stream.promises.pipeline(stream, fs.createWriteStream(outPath))
+      }
+
+      return `>>${id}\n` + segment("image", { url: `file://${outPath}` })
     })
 }
