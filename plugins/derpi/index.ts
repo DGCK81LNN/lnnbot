f4938a9acb64e411d8dc6771de0d3acb844fdeff
DGCK81LNN <triluolnn@163.com>
2022-03-23 11:09:37 +0800

    refactor(derpi)

--- plugins/derpi/index.ts
+++ plugins/derpi/index.ts
@@ -1,4 +1,4 @@
-import { Context, segment } from "koishi"
+import { Context, Session, segment } from "koishi"
 import fs from "fs"
 import Stream from "stream"
 import path from "path"
@@ -6,28 +6,64 @@ import axios from "axios"
 
 export const name = "lnnbot-derpi"
 export interface Config {
-  filter_id?: number,
+  filter_id?: number
 }
-export function apply(cxt: Context, config: Config = { }) {
-  cxt
-    .command("derpi <id:natural>", "获取呆站图片")
-    .action(async (_argv, id) => {
-      var outPath = path.resolve(`./.lnnbot_cache/${id}`)
+
+async function loadImage(session: Session, id: number, outPath: string) {
+  try {
+    await fs.promises.lstat(outPath)
+    return
+  } catch {}
+
+  await fs.promises.mkdir(path.dirname(outPath), { recursive: true })
+
+  try {
+    var metaResponse = await axios.get(`https://derpibooru.org/api/v1/json/images/${id}`)
+  } catch (err) {
+    throw session.text(".metadata-error")
+  }
+  var meta = metaResponse.data.image
+
+  try {
+    var imgResponse = await axios.get(meta.representations["small"], {
+      responseType: "stream",
+    })
+  } catch (err) {
+    throw session.text(".image-error")
+  }
+
+  var imgStream = imgResponse.data
+  await Stream.promises.pipeline(imgStream, fs.createWriteStream(outPath))
+}
+
+export function apply(ctx: Context, config: Config = {}) {
+  ctx
+    .command("derpi <id:natural>", {
+      checkArgCount: true,
+      checkUnknown: true,
+      showWarning: true,
+    })
+    .action(async ({ session }, id) => {
+      var outPath = path.resolve(`./.lnnbot_cache/${id}`).replace(/^\//, "")
 
       try {
-        await fs.promises.lstat(outPath)
-      } catch { // not cached
-        await fs.promises.mkdir(path.dirname(outPath), { recursive: true })
-
-        var metadata = (await axios.get(`https://derpibooru.org/api/v1/json/images/${id}`)).data.image
-
-        var stream = (await axios.get(metadata.representations["small"], {
-          responseType: "stream",
-        })).data
-
-        await Stream.promises.pipeline(stream, fs.createWriteStream(outPath))
+        await loadImage(session, id, outPath)
+      } catch (err) {
+        if (typeof err === "string") return err
+        throw err
       }
 
-      return `>>${id}\n` + segment("image", { url: `file://${outPath}` })
+      return (
+        segment("image", { url: `file:///${outPath}` }) +
+        `\nhttps://derpibooru.org/images/${id}`
+      )
     })
+
+  ctx.i18n.define("zh", "commands.derpi", {
+    description: "获取呆站图片",
+    messages: {
+      "metadata-error": "加载图片信息失败。",
+      "image-error": "加载图片失败。",
+    },
+  })
 }
